945536525ed58b637ee920c09ab20e70a0574ab8
Alejandro <alejandroxlllc@gmail.com>
2024-05-28 09:47:46 -0600

    Agregando Carpetas necesarias para el proyecto

---
+++ Frontend/front-gimnasio/src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '@/components/Login.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: LoginView
+    }
+   
+  ]
+})
+
+export default router
