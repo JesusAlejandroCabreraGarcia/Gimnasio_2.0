e1855cce79e68e87cf85fa7849927876697bec75
alejandro <alejandroxlllc@gmail.com>
2024-06-20 11:27:10 -0600

    Agregando el Backend

---
+++ Backend/routes/user.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from datetime import datetime
+
+user = APIRouter()
+users = []
+
+#userModel
+class model_user(BaseModel):
+    id:str
+    usuario:str
+    contrasena: str
+    created_at:datetime = datetime.now
+    estatus:bool=False
+
+
+@user.get('/')
+
+def bienvenida():
+    return 'Bienvenido al sistema'
+
+
+@user.get('/users')
+
+def get_usuarios():
+    return users
+
+
+@user.post('/users')
+
+def save_usuarios(users:model_user):
+    users.append
+    return "Datos guardados"
